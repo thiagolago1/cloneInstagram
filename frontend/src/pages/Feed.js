0fd5a269681aed59d3fcdffc45023261273a64cc
thiagolago <thiagolagoo2@gmail.com>
2019-06-12 19:17:57 -0300

    :construction: [Add] some new features

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 import './Feed.css';
 
 import more from '../assets/more.svg';
@@ -13,9 +14,31 @@ class Feed extends Component {
     };
 
     async componentDidMount() {
+        this.registerToSocket();
+
         const response = await api.get('posts'); //Chamando o backend
 
         this.setState({ feed: response.data });
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] })
+        })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post
+                )
+            })
+        })
     }
 
     render() {
@@ -36,7 +59,9 @@ class Feed extends Component {
 
                     <footer>
                         <div className="actions">
+                        <button type="button" onClick={() => this.handleLike(post._id)}>
                             <img src={like} alt="like" />
+                        </button>
                             <img src={comment} alt="comment" />
                             <img src={send} alt="send" />
                         </div>
